93854a33d652b35692312a4a071e7cbe70589781
leixw0102 <0102lei@163.com>
2014-09-25 10:18:07 +0800

    add parse line of command ,example publisher command : *.*.man.java -workers 5 -port 14630

--- logger-publisher/src/main/java/tv/icntv/logger/Main.java
+++ logger-publisher/src/main/java/tv/icntv/logger/Main.java
@@ -23,11 +23,16 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
-import tv.icntv.logger.msg.receive.*;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tv.icntv.logger.msg.receive.IConnection;
+import tv.icntv.logger.msg.receive.IReceiverAndSender;
+import tv.icntv.logger.msg.receive.MsgExecutor;
+import tv.icntv.logger.msg.receive.ScribeServer;
 import tv.icntv.logger.msg.send.ISender;
 import tv.icntv.logger.msg.send.KafkaClient;
-
-import java.lang.reflect.Constructor;
 
 /**
  * Created by leixw
@@ -37,32 +42,32 @@ import java.lang.reflect.Constructor;
  * Time: 14:10
  */
 public class Main {
-    private static final int DEFAULT_WORKERS = 10;
-    private static final int DEFAULT_PORT=14630;
+
+    private static String SCRIBE_WORKDER="workers";
+    private static String SCRIBE_PROTS="port";
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(final String[]args){
-         if(args.length>2){
-             System.out.println(" workers  ports");
-             return;
-         }
-
+        if(null == args || args.length==0){
+            logger.info("error args={}", args);
+            return ;
+        }
+        OptionParser parser = new Main().new ProducerConfig().getOptionParser();
+        final OptionSet optionSet = parser.parse(args);
+        if(null == parser){
+            return;
+        }
         Injector injector = Guice.createInjector(new Module() {
-            Integer ports=DEFAULT_PORT,workers=DEFAULT_WORKERS;
-
 
             @Override
             public void configure(Binder binder) {
-                if(args.length==1){
-                    ports=Ints.tryParse(args[0]);
-                } if(args.length==2){
-                    ports=Ints.tryParse(args[0]);
-                    workers=Ints.tryParse(args[1]);
-                }
-                binder.bind(Integer.class).annotatedWith(Names.named("workers")).toInstance(workers);
-                binder.bind(Integer.class).annotatedWith(Names.named("port")).toInstance(ports);
+
+                binder.bind(Integer.class).annotatedWith(Names.named(SCRIBE_WORKDER)).toInstance(Ints.tryParse(optionSet.valueOf(SCRIBE_WORKDER).toString()));
+                binder.bind(Integer.class).annotatedWith(Names.named(SCRIBE_PROTS)).toInstance(Ints.tryParse(optionSet.valueOf(SCRIBE_PROTS).toString()));
                 try {
-                    binder.bind(IConnection.class).toConstructor(ScribeServer.class.getConstructor(Integer.class,Integer.class));
+                    binder.bind(IConnection.class).toConstructor(ScribeServer.class.getConstructor(Integer.class, Integer.class));
                 } catch (NoSuchMethodException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    e.printStackTrace();
                     return;
                 }
                 binder.bind(ISender.class).to(KafkaClient.class);
@@ -74,5 +79,17 @@ public class Main {
 
     }
 
+    class ProducerConfig {
 
+        private static final int DEFAULT_WORKERS = 10;
+        private static final int DEFAULT_PORT=14630;
+        public  OptionParser getOptionParser(){
+
+            OptionParser parser = new OptionParser();
+            parser.accepts(SCRIBE_WORKDER).withOptionalArg().describedAs("start scribe server workers").ofType(Integer.class).defaultsTo(DEFAULT_WORKERS);
+
+            parser.accepts(SCRIBE_PROTS).withOptionalArg().defaultsTo("scribe server listener port").ofType(Integer.class).defaultsTo(DEFAULT_PORT);
+            return parser;
+        }
+    }
 }
